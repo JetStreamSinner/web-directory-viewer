44a80066bf9ad2a68040171115751276e9d9f5c3
Jetstream <bigalex934@gmail.com>
2022-05-24 19:26:47 +0300

    Create empty template index.html

--- source/main.py
+++ source/main.py
@@ -1,9 +1,21 @@
+import os
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
 
+load_dotenv()
 
 application = FastAPI()
 
+templates_dir = "templates"
+templates = Jinja2Templates(directory=templates_dir)
 
-@application.get("/")
-def root():
-    return "Root"
+
+@application.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(name="index.html", context={
+        "request": request
+    })
